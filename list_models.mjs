b8f682fb806eb213a2e3efc6a5b735fc4780c951
Nikhilsingh478 <neelsingh45940s@gmail.com>
2026-03-28 17:06:51 +0530

    clean up diagnostic test files

--- list_models.mjs
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'; // if node 18+, fetch is global
-const key = process.env.GEMINI_API_KEY_1 || "AIzaSyCWdy3WKNLb9oU1dL3wIrtTuB7drlpe23E";
-
-async function list() {
-  const res = await globalThis.fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`);
-  const data = await res.json();
-  console.log(data.models.map(m => m.name).join('\n'));
-}
-list();
